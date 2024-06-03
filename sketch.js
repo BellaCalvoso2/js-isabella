fe8c8eb7802e1b0ccd225c81dc2aa81a3a8c4f92
BellaCalvoso2 <isabellacalvoso@gmail.com>
2024-06-03 11:17:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 function setup(){
     creatwCanvas (600,600);
-    background ("black");
+}
+
+function draw() {
+      background ("black");
 }
 
 function draw() {
     stroke ("blue");
     FileList("red");
 
+    // console.log(mouseIsPressed)
+    
     if(mouseIsPressed){
         rect (mouseX, mouseY, 20, 35);
     }
